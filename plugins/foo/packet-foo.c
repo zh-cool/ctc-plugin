c5ad983391f80d0045dae5bfcee5bdb73540a778
austin <zhangyong@chinahisu.com>
2017-07-04 19:05:44 +0800

    foo

--- plugins/foo/packet-foo.c
+++ plugins/foo/packet-foo.c
@@ -6,28 +6,109 @@
 #define FOO_PORT 1234
 
 static int proto_foo = -1;
+static int hf_pdu_type = -1;
+static int hf_foo_flag = -1;
+static int hf_foo_seq = -1;
+static int hf_foo_initialip;
 
+static gint ett_foo = -1;
+
+static const value_string pktname[] = {
+        {1, "Initial"},
+        {2, "Terminate"},
+        {3, "Data"},
+        {0, NULL}
+};
 
 static int
 dissect_foo(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *data _U_)
 {
-    col_set_str(pinfo->cinfo, COL_PROTOCOL, "FOOT");
-    /* Clear out stuff in the info column */
-    col_clear(pinfo->cinfo,COL_INFO);
 
-    fprintf(stderr, "Entry func:%s\n", __func__);
-    return tvb_captured_length(tvb);
+        gint offset = 0;
+        guint8 packet_type = tvb_get_guint8(tvb, 0);
+
+
+        col_set_str(pinfo->cinfo, COL_PROTOCOL, "FOOT");
+        /* Clear out stuff in the info column */
+        col_clear(pinfo->cinfo,COL_INFO);
+
+        col_add_fstr(pinfo->cinfo, COL_INFO, "Type %s", val_to_str(packet_type, pktname, "Unknown (0x%02x)"));
+
+        proto_item *ti = proto_tree_add_item(tree, proto_foo, tvb, 0, -1, ENC_NA);
+        proto_item_append_text(ti, ",Type %s", val_to_str(packet_type, pktname, "Unknown (0x%02x)"));
+        proto_tree *foo_tree = proto_item_add_subtree(ti, ett_foo);
+
+        proto_tree_add_item(foo_tree, hf_pdu_type, tvb, offset, 1, ENC_BIG_ENDIAN);
+        offset += 1;
+
+        proto_tree_add_item(foo_tree, hf_foo_flag, tvb, offset, 1, ENC_BIG_ENDIAN);
+        offset += 1;
+
+        proto_tree_add_item(foo_tree, hf_foo_seq, tvb, offset, 2, ENC_BIG_ENDIAN);
+        offset += 1;
+
+        proto_tree_add_item(foo_tree, hf_foo_initialip, tvb, offset, 4, ENC_BIG_ENDIAN);
+        offset += 4;
+
+        return tvb_captured_length(tvb);
 }
 
 void
 proto_register_foo(void)
 {
+        static hf_register_info hf[] = {
+                {
+                        &hf_pdu_type,
+                        {
+                                "FOO PDU Type",
+                                "foo.type",
+                                FT_UINT8, BASE_DEC,
+                                VALS(pktname), 0x0,
+                                NULL, HFILL
+                        }
+                },
+                {
+                        &hf_foo_flag,
+                        {
+                                "FOO PDU Flag",
+                                "foo.flag",
+                                FT_UINT8, BASE_HEX,
+                                NULL, 0x0,
+                                NULL, HFILL
+                        }
+                },
+                {
+                        &hf_foo_seq,
+                        {
+                                "FOO PDU seq", "foo.seq",
+                                FT_UINT16, BASE_DEC,
+                                NULL, 0x0,
+                                NULL, HFILL
+                        }
+                },
+                {
+                        &hf_foo_initialip,
+                        {
+                                "FOO PDU Init IP", "foo.initIP",
+                                FT_IPv4, BASE_NONE,
+                                NULL, 0x0,
+                                NULL, HFILL
+                        }
+                }
+        };
+
+        static gint *ett[] = {
+                &ett_foo
+        };
+
         proto_foo = proto_register_protocol (
                         "FOO Protocol", /* name       */
                         "FOO",      /* short name */
                         "foo"       /* abbrev     */
                         );
-    fprintf(stderr, "Entry func:%s\n", __func__);
+        
+        proto_register_field_array(proto_foo, hf, array_length(hf));
+        proto_register_subtree_array(ett, array_length(ett));
 }
 
 void
